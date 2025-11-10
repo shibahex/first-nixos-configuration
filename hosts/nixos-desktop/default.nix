938a8cb2b9326ebe84b2f446f94ca6c8122251d2
shibahex <badamsva@gmail.com>
2025-11-10 21:14:42 +0000

    add monitors to config

--- hosts/nixos-desktop/default.nix
+++ hosts/nixos-desktop/default.nix
@@ -1,9 +1,5 @@
-{ pkgs, ... }:
-{
-  imports = [
-    ./hardware.nix
-    ./host-packages.nix
-  ];
+{ ... }: {
+  imports = [ ./hardware.nix ./host-packages.nix ];
   system.stateVersion = "25.05";
   nixpkgs.config.allowUnfree = true;
 }
--- hosts/nixos-desktop/variables.nix
+++ hosts/nixos-desktop/variables.nix
@@ -1,4 +1,4 @@
-{
+{ pkgs }: {
   # Git Configuration
   gitUsername = "shiba";
   gitEmail = "shiba@nixos-desktop";
@@ -9,8 +9,19 @@
   nvidiaID = "PCI:1:0:0"; # Update with your NVIDIA GPU ID
 
   # Startup Applications
-  startupApps = [
-  ];
+  startupApps = [ ];
 
- defaultShell = "nushell";
+  defaultShell = "nushell";
+  monitorRules = [{
+    name = "Gaming monitors";
+    outputs_connected = [ "HDMI-0" "DP-2" ];
+    configure_single = "HDMI-0";
+    primary = true;
+    atomic = true;
+    execute_after = [
+      "${pkgs.xorg.xrandr}/bin/xrandr --output HDMI-0 --mode 2560x1440 --rate 143.91 --primary"
+      "${pkgs.xorg.xrandr}/bin/xrandr --output DP-2 --mode 2560x1440 --rate 100 --right-of HDMI-0 --rotate right"
+    ];
+  }];
+
 }
