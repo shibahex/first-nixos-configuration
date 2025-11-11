31e14725262e7e14fce209aa6c3d4a462c526c47
shibahex <badamsva@gmail.com>
2025-11-10 21:01:51 -0500

    add: create new module for nix/system settings ad add timezone

---
+++ modules/core/nix-system.nix
@@ -0,0 +1,18 @@
+# For any nix/system configuration
+{ host, pkgs, ... }:
+let
+  inherit (import ../../hosts/${host}/variables.nix { pkgs = pkgs; }) timeZone;
+in
+{
+  nix = {
+    settings = {
+      download-buffer-size = 250000000;
+      auto-optimise-store = true;
+      experimental-features = [ "nix-command" "flakes" ];
+    };
+  };
+  time.timeZone = timeZone;
+  i18n.defaultLocale = "en_US.UTF-8";
+  system.stateVersion = "25.05";
+  nixpkgs.config.allowUnfree = true;
+}
