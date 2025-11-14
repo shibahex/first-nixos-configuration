f362589ddaa78de325c7350acf99a7c677cd1451
shibahex <badamsva@gmail.com>
2025-11-13 22:38:43 -0500

    add easyeffects

--- modules/home-apps/easyeffects.nix
+++ modules/home-apps/easyeffects.nix
@@ -1,6 +1,16 @@
 # The goal of this module is to add easyeffects presets from git into the .config file
-{ host, pkgs, ... }:
+{ pkgs, ... }:
+let
+  easyEffectsProfiles = pkgs.fetchFromGitHub {
+    owner = "shibahex";
+    repo = "easyeffects-presets";
+    sha256 = "1q22z0q0pbp5j67aahxaf6byjfrzsv39szzbn7h3vmf77g10nb0v";
+    rev = "54e3dde1eba2f8aaf97d85f80eb95bb72f2fcca8";
+  };
+in
 {
-  #54e3dde1eba2f8aaf97d85f80eb95bb72f2fcca8
-  #sha 1q22z0q0pbp5j67aahxaf6byjfrzsv39szzbn7h3vmf77g10nb0v
+  home.file.".config/easyeffects" = {
+    source = easyEffectsProfiles;
+    recursive = true;
+  };
 }
